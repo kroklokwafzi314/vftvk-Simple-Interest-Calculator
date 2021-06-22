4141fa1d69449da93ae6fc638570e844708ea580
kroklokwafzi314 <rhecht@gmx.at>
2021-06-22 08:22:50 +0000

    done

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    rate = document.getElementById("rate").value;
-    years = document.getElementById("years").value;
-    var res = p * (1 + (rate /100))**years;
-    var resFormat = res.toFixed(2);
-    var targetYear = 2020 + years;
-    document.getElementById("result").innerHTML = "If you deposit " + p +",<br>"
-      + "at an interest rate of " + rate + "%.<br>"
-      + "You will receive an amount of " + resFormat + ",<br>"
-      + "in the year " + targetYear;
+    var principal = document.getElementById("principal").value;
+    if (!validate(principal, "Amount", "principal")) {
+        return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var res = principal * years * rate / 100;
+    var targetYear = new Date().getFullYear() + parseInt(years);
+    //alert("prinicipal " + principal + ", targetYear " + targetYear + ", res " + resFormat);
+    document.getElementById("result").innerHTML = 
+        "If you deposit <span class='highlight'>" + principal +"</span>,<br>"
+      + "at an interest rate of <span class='highlight'>" + rate + "%</span>.<br>"
+      + "You will receive an amount of <span class='highlight'>" 
+      + res + "</span>,<br>"
+      + "in the year <span class='highlight'>" + targetYear + "</span>"
+      ;
 }
 
+//update rate-val when rate is changed.
 function rateChanged()
 {
     rate = document.getElementById("rate").value;
     document.getElementById("rate-val").innerHTML = rate + "%";
 }
-        
+
+//validation for val - if value is null, displays a message telling the user
+//to enter a value for name (label of the element) and sets focus to id (id of the element).
+function validate(val, name, id) 
+{
+    if ((val === undefined || val == null || val.length <= 0)) {
+        alert("Please enter a value for " + name);
+        document.getElementById(id).focus();
+        return false;
+    }
+    return true;
+}
